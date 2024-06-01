c68b50e0754152b59ac9068704acf2fd8467de9c
Ajuatah <ajuatahcodingarena@gmail.com>
2024-06-01 19:13:21 +0100

    modify models

---
+++ KBankATM/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBankATM.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public bool IsKBankCustomer { get; set; }
+        public decimal Balance { get; set; }
+        public string Pin { get; set; }
+        public List<Transaction> Transactions { get; set; }
+
+        public Customer(string name, bool isKBankCustomer, decimal balance, string pin)
+        {
+            Name = name;
+            IsKBankCustomer = isKBankCustomer;
+            Balance = balance;
+            Pin = pin;
+            Transactions = new List<Transaction>();
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            Transactions.Add(transaction);
+        }
+    }
+}
---
+++ KBankATM/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using KBankATM.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBankATM.Models
+{
+    public class Transaction
+    {
+        public TransactionType Type { get; set; }
+        public decimal Amount { get; set; }
+        public decimal BalanceAfter { get; set; }
+
+        public Transaction(TransactionType type, decimal amount, decimal balanceAfter)
+        {
+            Type = type;
+            Amount = amount;
+            BalanceAfter = balanceAfter;
+        }
+    }
+
+}
